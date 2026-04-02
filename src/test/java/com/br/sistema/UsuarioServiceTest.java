7a411568c3e39da7271c16ece986c6a99c70600a
Pedro <thunder.pedro@hotmail.com>
2026-04-02 18:21:16 +0100

    Teste github actions

--- src/test/java/com/br/sistema/UsuarioServiceTest.java
+++ src/test/java/com/br/sistema/UsuarioServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,6 +20,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+
+@ActiveProfiles("usuarios")
 @ExtendWith(MockitoExtension.class)
 class UsuarioServiceTest {
 
